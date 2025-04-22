eed8cc75b82964037109775c15064a49b9ffba0c
jfr9910rit <jfr9910@rit.edu>
2025-04-22 11:46:29 -0400

    temp, dont pull to main

--- Eye_On_The_Prize/Assets/Scripts/EndScript.cs
+++ Eye_On_The_Prize/Assets/Scripts/EndScript.cs
@@ -1,5 +1,10 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class EndScript : MonoBehaviour
 {
@@ -8,27 +13,86 @@ public class EndScript : MonoBehaviour
     
 
     public TextMeshProUGUI[] pScores;
+    public Image[] podiums;
+    public Image[] glows;
+    public Image[] chars;
+    public VideoPlayer outro;
+    private float outroTime;
 
     void Awake()
     {
-        Debug.Log("end scene");
-        GameManager.Instance.isTimerRunning = false;
-        
+        outroTime = 10f;
+        for(int i = 0; i < GameManager.Instance.playerCount; i++)
+        {
+            //podiums[i].setActive(false);
+            //glows[i].setActive(false);
+            //chars[i].setActive(false);
+            //pScores[i].setActive(false);
+        }
 
 
+        //GameManager.Instance.isTimerRunning = false;
 
+        //if (GameManager.Instance.playerCount == 1)
+        //{
+        //    if (GameManager.Instance.pRoles[0] == "eppee")
+        //    {
+                
+        //    }
+        //    else if (GameManager.Instance.pRoles[0] == "teebee")
+        //    {
+                
+        //    }
+        //    else if (GameManager.Instance.pRoles[0] == "heartly")
+        //    {
+                
+        //    }
 
         //}
+        //else if (GameManager.Instance.playerCount == 2)
+        //{
+        //    if (playerInt == 0)
+        //    {
+        //        if (GameManager.Instance.pRoles[0] == "eppee")
+        //        {
+                    
+        //        }
+        //        else if (GameManager.Instance.pRoles[0] == "teebee")
+        //        {
+                    
+        //        }
+        //    }
+        //    else if (playerInt == 1)
+        //    {
+        //        if (GameManager.Instance.pRoles[1] == "teebee")
+        //        {
+                    
+        //        }
+        //        else if (GameManager.Instance.pRoles[1] == "heartly")
+        //        {
+                    
+        //        }
+        //    }
+        //}
+        //else if (GameManager.Instance.playerCount == 3)
+        //{
+
+        //}
+
+
+
+
         if (GameManager.Instance.difficultyLevel == 2)
         {
-            EndBanner.text = "Game Over \nPress any button to Restart";
+            EndBanner.text = "Game Over";
+
         }
         else {
             EndBanner.text = "Round " + (GameManager.Instance.difficultyLevel + 1) + " Over";
         }
         for(int i = 0; i < GameManager.Instance.playerCount; i++)
         {
-            pScores[i].text = "Player " + (i +1) +  "\nScore: " + GameManager.Instance.playerPoints[i, 1];
+            pScores[i].text = /*"Player " + (i +1) +  */"Score: " + GameManager.Instance.playerPoints[i, 1];
         }
         
     }
@@ -36,6 +100,10 @@ public class EndScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        outroTime -= Time.deltaTime;
+        if(outroTime == 0)
+        {
+            //Debug.Log("Done!");
+        }
     }
 }
